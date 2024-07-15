9c9e784ce4730eb8f8735595776c74496b0dbe5f
Mario Loera <marl@trustpilot.com>
2024-07-15 14:45:43 +0200

    work

--- yaml_schemas/test_schema.py
+++ yaml_schemas/test_schema.py
@@ -6,35 +6,11 @@ class TestSchemas:
     SCHEMAS = [f"{PATH}/definition.yaml"]
 
     def test_good_schema(self):
-        yamls = [
-            f"{self.PATH}/good_example.yaml",
-        ]
-
-        for data in yamls:
-            print(data)
-            c = Core(source_file=data, schema_files=self.SCHEMAS)
-            c.validate(raise_exception=True)
+        data = f"{self.PATH}/good_example.yaml"
+        c = Core(source_file=data, schema_files=self.SCHEMAS)
+        c.validate(raise_exception=True)
 
     def test_bad_schema(self):
-        schemas = [f"{self.PATH}/definition.yaml"]
-        yamls = [
-            f"{self.PATH}/bad_example.yaml",
-        ]
-
-        for data in yamls:
-            print(data)
-            c = Core(source_file=data, schema_files=self.SCHEMAS)
-            # c.validate(raise_exception=True)
-
-# def test_schema():
-#     path = "./yaml_schemas/foo"
-#     schemas = [f"{path}/definition.yaml"]
-#     yamls = [
-#         f"{path}/good_example.yaml",
-#         f"{path}/bad_example.yaml",
-#     ]
-
-#     for data in yamls:
-#         print(data)
-#         c = Core(source_file=data, schema_files=schemas)
-#         #c.validate(raise_exception=True)
+        data = f"{self.PATH}/bad_example.yaml"
+        c = Core(source_file=data, schema_files=self.SCHEMAS)
+        c.validate(raise_exception=True)
